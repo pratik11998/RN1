5b75ea1c08fcdba2e11d20eb3ed204ba17ea8b34
pratik11998 <74367702+pratik11998@users.noreply.github.com>
2020-12-08 18:03:13 +0530

    done

--- AwesomeProj/pages/SecondPage.js
+++ AwesomeProj/pages/SecondPage.js
@@ -2,8 +2,13 @@ import React, { Component } from 'react';
 //import react in our code. 
 import { StyleSheet, View, Text,Button} from 'react-native';
 //import all the components we are going to use.
-
+import Camera from 'react-native-camera';
 export default class SecondPage extends Component {
+  takePicture() {  
+    this.camera    
+    .capture()    
+    .then((data) => console.log(data))    
+    .catch(err => console.error(err));}
   static navigationOptions = {
     title: 'Second Page',
     //Sets Header text of Status Bar
@@ -21,7 +26,9 @@ export default class SecondPage extends Component {
   render() {
     const { navigate } = this.props.navigation;
     return (
+      
       <View style={styles.container}>
+        <Camera  ref={cam => {this.camera = cam}}  style={styles.preview}  aspect={Camera.constants.Aspect.fill}>  <Text style={styles.capture} onPress={this.takePicture.bind(this)}>    [CAPTURE]  </Text></Camera>
       <Text>profile</Text>
         <Button title='Logout' onPress={() =>navigate('ThirdPage')}/>
       </View>
@@ -34,5 +41,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
-});
+  },preview: {  flex: 1,  justifyContent: 'flex-end',  alignItems: 'center',  height: Dimensions.get('window').height,  width: Dimensions.get('window').width},  capture: {    flex: 0,    backgroundColor: '#fff',    borderRadius: 5,    color: '#000',    padding: 10,    margin: 40    }},
+);
