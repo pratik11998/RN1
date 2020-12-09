ef09b0b13d8f5d47789e98c61e0b917b89e0737b
pratik11998 <74367702+pratik11998@users.noreply.github.com>
2020-12-09 17:09:12 +0530

    done

--- AwesomeProj/pages/SecondPage.js
+++ AwesomeProj/pages/SecondPage.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 //import react in our code. 
 import { StyleSheet, View, Text,Button} from 'react-native';
-//import all the components we are going to use.
-import Camera from 'react-native-camera';
+import { Camera ,} from 'react-native-camera';
 export default class SecondPage extends Component {
-  takePicture() {  
-    this.camera    
-    .capture()    
-    .then((data) => console.log(data))    
-    .catch(err => console.error(err));}
+  
+  takePicture = () => {
+    const options = {};
+    this.camera.capture({ metadata: options })
+    .then((data) => console.log(data))
+    .catch(err => console.error(err));
+ }
   static navigationOptions = {
     title: 'Second Page',
     //Sets Header text of Status Bar
@@ -28,7 +29,14 @@ export default class SecondPage extends Component {
     return (
       
       <View style={styles.container}>
-        <Camera  ref={cam => {this.camera = cam}}  style={styles.preview}  aspect={Camera.constants.Aspect.fill}>  <Text style={styles.capture} onPress={this.takePicture.bind(this)}>    [CAPTURE]  </Text></Camera>
+        <Camera
+               ref = {(cam) => {
+                  this.camera = cam;
+               }}
+               style = {styles.preview}
+               aspect = {Camera.constants.Aspect.fill}>
+            </Camera>
+            <Text style = {styles.capture} onPress = {this.takePicture}>CAPTURE</Text>
       <Text>profile</Text>
         <Button title='Logout' onPress={() =>navigate('ThirdPage')}/>
       </View>
@@ -38,8 +46,16 @@ export default class SecondPage extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },preview: {  flex: 1,  justifyContent: 'flex-end',  alignItems: 'center',  height: Dimensions.get('window').height,  width: Dimensions.get('window').width},  capture: {    flex: 0,    backgroundColor: '#fff',    borderRadius: 5,    color: '#000',    padding: 10,    margin: 40    }},
-);
+
+  },
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center'
+ },
+ capture: {
+    fontSize: 30,
+    color: 'red',
+    alignSelf: 'center',
+ }
+});
