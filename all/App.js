3057b7177088c54c0694ce94c66b0404778e9c15
pratik11998 <74367702+pratik11998@users.noreply.github.com>
2021-01-04 17:57:35 +0530

    done

--- all/App.js
+++ all/App.js
@@ -1,24 +1,67 @@
+// import * as React from 'react';
+// import DraweerMenu from './pages/DraweerMenu'
+// import HomeComponent from './pages/HomeComponent';
+// import LoginScreen from './src/screens';
+// import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
+// const theme = {
+//   ...DefaultTheme,
+//   roundness: 2,
+//   colors: {
+//     ...DefaultTheme.colors,
+//     primary: '#3498db',
+//     accent: '#f1c40f',
+//   },
+// };
+
+// const App = () => {
+//   return (
+//  // <DraweerMenu/>
+  
+//   <LoginScreen/>
+//      // <DraweerMenu/>
+   
+//   );
+// }
+// export default App;
 import * as React from 'react';
-import DraweerMenu from './pages/DraweerMenu'
-import HomeComponent from './pages/HomeComponent';
-import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
-const theme = {
-  ...DefaultTheme,
-  roundness: 2,
-  colors: {
-    ...DefaultTheme.colors,
-    primary: '#3498db',
-    accent: '#f1c40f',
-  },
-};
+import { Provider } from 'react-native-paper'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import { theme } from './src/core/theme'
+import LoginComponent from './pages/LoginComponent';
+import {
+  StartScreen,
+  LoginScreen,
+  RegisterScreen,
+  ForgotPasswordScreen,
+  Dashboard,
+} from './src/screens'
+
+const Stack = createStackNavigator()
 
 const App = () => {
   return (
- // <DraweerMenu/>
-  
-  
-      <DraweerMenu/>
-   
-  );
+    <Provider theme={theme}>
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="LoginScreen"
+          screenOptions={{
+            headerShown: false,
+          }}
+        >
+          <Stack.Screen name="StartScreen" component={StartScreen} />
+          <Stack.Screen name="LoginScreen" component={LoginScreen} />
+          <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
+          <Stack.Screen name="Dashboard" component={Dashboard} />
+          <Stack.Screen name="login2" component={LoginComponent}/>
+          <Stack.Screen
+            name="ForgotPasswordScreen"
+            component={ForgotPasswordScreen}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
+  )
 }
-export default App;
+
+export default App
--- all/pages/LoginComponent.js
+++ all/pages/LoginComponent.js
@@ -1,12 +1,126 @@
-import React from 'react'
-import { Text, Button } from 'react-native'
-const LoginComponent = ({ navigation }) => {
+import React from 'react';
+import {StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  TouchableHighlight,
+  Image,
+  Alert} from 'react-native';
+
+export default class App extends React.Component {
+  static navigationOptions = {
+    title: 'Login',
+    //Sets Header text of Status Bar
+    headerStyle: {
+      backgroundColor: '#f4511e',
+      //Sets Header color
+    },
+    headerTintColor: '#fff',
+    //Sets Header text color
+    headerTitleStyle: {
+      fontWeight: 'bold',
+      //Sets Header text style
+    },
+  };
+ 
+  state={
+    email:null,
+    password:null
+  }
+    Loginfu(){
+ const { navigate } = this.props.navigation;
+   
+        navigate('FirstPage');
+    
+      }
+      onClickListener = (viewId) => {
+        Alert.alert("Alert", "Button pressed "+viewId);
+      }
+
+
+ 
+  render(){
+    const { navigate } = this.props.navigation;
     return (
-        <>
-            <Text>From Login Componnet</Text>
-            <Button title="Open Drawer" onPress={() => navigation.openDrawer()}></Button>
-        </>
-    )
+      <View style={styles.container}>
+        <View style={styles.inputContainer}>
+          <Image style={styles.inputIcon} source={{uri: 'https://png.icons8.com/message/ultraviolet/50/3498db'}}/>
+          <TextInput style={styles.inputs}
+              placeholder="Email"
+              keyboardType="email-address"
+              underlineColorAndroid='transparent'
+              onChangeText={(email) => this.setState({email})}/>
+        </View>
+        
+        <View style={styles.inputContainer}>
+          <Image style={styles.inputIcon} source={{uri: 'https://png.icons8.com/key-2/ultraviolet/50/3498db'}}/>
+          <TextInput style={styles.inputs}
+              placeholder="Password"
+              secureTextEntry={true}
+              underlineColorAndroid='transparent'
+              onChangeText={(password) => this.setState({password})}/>
+        </View>
+
+        <TouchableHighlight style={[styles.buttonContainer, styles.loginButton]} onPress={ this.Loginfu.bind(this) }>
+          <Text style={styles.loginText}>Login</Text>
+        </TouchableHighlight>
+
+        <TouchableHighlight style={styles.buttonContainer} onPress={() => this.onClickListener('restore_password')}>
+            <Text>Forgot your password?</Text>
+        </TouchableHighlight>
+
+        <TouchableHighlight style={styles.buttonContainer} onPress={() => this.onClickListener('register')}>
+            <Text>Register</Text>
+        </TouchableHighlight>
+      </View>
+    );
+  }
 }
 
-export default LoginComponent;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#DCDCDC',
+  },
+  inputContainer: {
+      borderBottomColor: '#F5FCFF',
+      backgroundColor: '#FFFFFF',
+      borderRadius:30,
+      borderBottomWidth: 1,
+      width:250,
+      height:45,
+      marginBottom:20,
+      flexDirection: 'row',
+      alignItems:'center'
+  },
+  inputs:{
+      height:45,
+      marginLeft:16,
+      borderBottomColor: '#FFFFFF',
+      flex:1,
+  },
+  inputIcon:{
+    width:30,
+    height:30,
+    marginLeft:15,
+    justifyContent: 'center'
+  },
+  buttonContainer: {
+    height:45,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom:20,
+    width:250,
+    borderRadius:30,
+  },
+  loginButton: {
+    backgroundColor: "#00b5ec",
+  },
+  loginText: {
+    color: 'white',
+  }
+});
